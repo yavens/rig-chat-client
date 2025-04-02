af20a74afadd2b456f1051b07a05a3b4c90a2646
yavens <179155341+yavens@users.github.noreply.github.com>
2025-04-01 20:15:51 -0400

    feat: fill out prompt state

--- src/state/prompt.rs
+++ src/state/prompt.rs
@@ -1,9 +1,11 @@
 use std::time::SystemTime;
 
 use actix_web_lab::sse;
+use askama::Template;
 use rig::completion::Message;
 use tokio::sync::mpsc::Sender;
 
+use crate::templates::{ChatHistoryTemplate, MessageTemplate};
 
 #[derive(Default)]
 pub struct PromptState {
@@ -13,15 +15,63 @@ pub struct PromptState {
 
 impl PromptState {
     pub async fn replace_chat_history(&self) {
-        todo!()
+        let _ = self
+            .send(
+                sse::Data::new(
+                    ChatHistoryTemplate {
+                        messages: self.messages.clone(),
+                    }
+                    .render()
+                    .expect("Failed to render chat history"),
+                )
+                .event("chat_history")
+                .into(),
+            )
+            .await;
     }
-
+    
     pub async fn send_message(&mut self, message: Message) -> usize {
-        todo!()
+        let index = self.messages.len();
+        self.messages.push(message.clone());
+
+        let data = MessageTemplate { index, message }
+            .render()
+            .expect("Failed to render message");
+
+        let _ = self
+            .send(sse::Data::new(data).event("new_message").into())
+            .await;
+
+        index
     }
 
     pub async fn update_message(&mut self, index: usize, chunk: String) {
-        todo!()
+        let message = &self.messages[index];
+
+        let new_message = match message {
+            Message::User { content } => match content.first() {
+                rig::message::UserContent::Text(text) => {
+                    Message::user(format!("{}{}", text.text, chunk))
+                }
+                _ => panic!("Can't update non-text message"),
+            },
+            Message::Assistant { content } => match content.first() {
+                rig::message::AssistantContent::Text(text) => {
+                    Message::assistant(format!("{}{}", text.text, chunk))
+                }
+                _ => panic!("Can't update non-text message"),
+            },
+        };
+
+        self.messages[index] = new_message;
+
+        let _ = self
+            .send(
+                sse::Data::new(format!(r#"<span class="text">{chunk}</span>"#))
+                    .event(format!("update_message#{}", index))
+                    .into(),
+            )
+            .await;
     }
 
     pub async fn queue_audio(&self, generation_time: SystemTime) {
